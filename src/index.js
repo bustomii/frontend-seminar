bb43265396555933995a98a623884a0be0d41add
bustomii <tomblok.id@gmail.com>
2022-04-14 13:51:41 +0700

    Push server V2

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { Provider } from "react-redux";
 import configureStore from './store'
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios'
-const URL = 'http://127.0.0.1:4000/' //local
-// const URL = 'http://seminar.astronacci.com:4000/' //server
+// const URL = 'http://127.0.0.1:4000/' //local
+const URL = 'http://seminar.astronacci.com:4000/' //server
 
 const REACT_VERSION = React.version;
 
